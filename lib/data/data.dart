93c3022b10836b97c78d53a8bbeaabd3e902db45
CaioCoelhoChaves <caiocoelhochaves@gmail.com>
2025-09-25 00:04:31 -0300

    Export files improvement

---
+++ lib/data/data.dart
@@ -0,0 +1,6 @@
+export 'dependency_injection.dart';
+export 'parser/entity_parser.dart';
+export 'parser/to_entity_list_parser.dart';
+export 'repository/dio_repository.dart';
+export 'repository/sqflite_repository.dart';
+export 'typedefs.dart';
---
+++ lib/domain/domain.dart
@@ -0,0 +1,4 @@
+export 'entity/entity.dart';
+export 'repository/mixin/create.dart';
+export 'repository/mixin/get_all.dart';
+export 'repository/repository.dart';

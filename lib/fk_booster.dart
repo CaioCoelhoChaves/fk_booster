a7f785d583320c2393623fcb6750181e8e5924b5
CaioCoelhoChaves <caiocoelhochaves@gmail.com>
2025-09-25 00:02:01 -0300

    Updated export file

--- lib/fk_booster.dart
+++ lib/fk_booster.dart
@@ -1,4 +1,18 @@
 export 'package:get_it/get_it.dart';
 export 'package:go_router/go_router.dart';
 export 'package:signals/signals_flutter.dart';
+
+export 'data/dependency_injection.dart';
+export 'data/parser/entity_parser.dart';
+export 'data/parser/to_entity_list_parser.dart';
+export 'data/repository/dio_repository.dart';
+export 'data/repository/sqflite_repository.dart';
+export 'data/typedefs.dart';
+export 'domain/entity/entity.dart';
+export 'domain/repository/mixin/create.dart';
+export 'domain/repository/mixin/get_all.dart';
+export 'domain/repository/repository.dart';
+export 'presentation/command.dart';
+export 'presentation/view_model.dart';
+export 'presentation/view_state.dart';
 export 'widgets/gap.dart';
